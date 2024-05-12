18b45112467551bd3ac723a895eced5312b34ca4
Systemx <607432@qq.com>
2024-05-12 09:54:01 +0800

    Added default config in config.cs

---
+++ Source/X.Helper.Cache/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X.Helper.Cache
+{
+    public static class Config
+    {
+        public static double Timeout { get; set; } = 20;
+
+        private static int _DefaultDatabase = 0;
+        public static int DefaultDatabase
+        {
+            get
+            {
+                return _DefaultDatabase;
+            }
+            set
+            {
+                var index = 0;
+                if (value >= 0 && value <= 15)
+                    index = value;
+                _DefaultDatabase = index;
+            }
+        }
+
+        public static int MaxReadPoolSize { get; set; } = 100;
+        public static int MaxWritePoolSize { get; set; } = 100;
+        public static bool AutoStart { get; set; } = true;
+    }
+}
