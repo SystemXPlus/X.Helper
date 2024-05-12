dbd2f58ee9928413b390952a4da374f1eb1ffb0d
Systemx <607432@qq.com>
2024-05-12 10:31:34 +0800

    Added default config in config.cs

--- Source/X.Helper.Cache/Config.cs
+++ Source/X.Helper.Cache/Config.cs
@@ -26,6 +26,65 @@ namespace X.Helper.Cache
             }
         }
 
+        private static string _RedisServer;
+
+        public static string RedisServer
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_RedisServer))
+                {
+                    var server = System.Configuration.ConfigurationManager.AppSettings["RedisServer"];
+                    if (string.IsNullOrEmpty(server))
+                    {
+                        server = "localhost";
+                    }
+                    _RedisServer = server;
+                }
+                return _RedisServer;
+            }
+            set { _RedisServer = value; }
+        }
+
+        private static string _RedisPort;
+
+        public static string RedisPort
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_RedisPort))
+                {
+                    var port = System.Configuration.ConfigurationManager.AppSettings["RedisPort"];
+                    if (string.IsNullOrEmpty(port))
+                    {
+                        port = "6379";
+                    }
+                    _RedisPort = port;
+                }
+                return _RedisPort;
+            }
+            set { _RedisPort = value; }
+        }
+
+        private static string _RedisPassword = null;
+
+        public static string RedisPassword
+        {
+            get
+            {
+                if (null == _RedisPassword)
+                {
+                    var password = System.Configuration.ConfigurationManager.AppSettings["RedisPassword"];
+                    if (string.IsNullOrEmpty(password))
+                        password = string.Empty;
+                    _RedisPassword = password;
+                }
+                return _RedisPassword;
+            }
+            set { _RedisPassword = value; }
+        }
+
+
         public static int MaxReadPoolSize { get; set; } = 100;
         public static int MaxWritePoolSize { get; set; } = 100;
         public static bool AutoStart { get; set; } = true;
