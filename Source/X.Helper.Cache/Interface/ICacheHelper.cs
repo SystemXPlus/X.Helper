7219a1bf102f1312efac71b4046d5b18de4dd4bd
Systemx <607432@qq.com>
2024-05-12 09:30:25 +0800

    Added interface ICacheHelper

--- Source/X.Helper.Cache/Interface/ICacheHelper.cs
+++ Source/X.Helper.Cache/Interface/ICacheHelper.cs
@@ -1,9 +1,67 @@
-﻿using System;
+﻿using ServiceStack.Script;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace X.Helper.Cache.Interface
 {
+    /// <summary>
+    /// 缓存HELPER类接口
+    /// </summary>
     public interface ICacheHelper
     {
+        /// <summary>
+        /// 设置要使用的数据库索引
+        /// </summary>
+        /// <param name="index">索引号：0-15（默认0）</param>
+        /// <returns></returns>
+        ICacheHelper GetDatabase(int index);
+        /// <summary>
+        /// 查询指定KEY是否存在
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        bool ExistsKey(string key);
+        /// <summary>
+        /// 移除指定KEY
+        /// </summary>
+        /// <param name="key">KEY NAME, Supports wildcard:*</param>
+        /// <returns></returns>
+        bool RemoveKey(string key);
+        /// <summary>
+        /// 设置指定KEY的对象
+        /// </summary>
+        /// <typeparam name="T">typeof cache object</typeparam>
+        /// <param name="key">key name</param>
+        /// <param name="obj">cache object</param>
+        /// <returns></returns>
+        bool SetValue<T>(string key, T obj) where T : class;
+        /// <summary>
+        /// 设置指定KEY的对象
+        /// </summary>
+        /// <typeparam name="T">typeof cache object</typeparam>
+        /// <param name="key">KEY</param>
+        /// <param name="obj">缓存对象</param>
+        /// <param name="timeout">缓存超时时间</param>
+        /// <returns></returns>
+        bool SetValue<T>(string key, T obj, double timeout) where T : class;
+        /// <summary>
+        /// 批量写入
+        /// </summary>
+        /// <typeparam name="T">typeof cache object</typeparam>
+        /// <param name="dic">KEY和缓存对象的字典</param>
+        /// <returns></returns>
+        bool SetValue<T>(IDictionary<string, T> dic) where T : class;
 
+        T Getvalu<T>(string key) where T : class;
+
+        T GetValue<T>(string key, double timeout) where T : class;
+
+        T GetValue<T>(string key, Func<T> func) where T : class;
+
+        T GetValue<T>(string key, Func<T> func, bool renewal = false) where T : class;
+
+        T GetValue<T>(string key, Func<T> func, double timeout, bool renewal = false) where T : class;
     }
 }
