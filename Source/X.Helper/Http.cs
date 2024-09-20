c75b676573b400389da2f1402f59fb3da2a5f24d
xf <607432@qq.com>
2024-09-20 14:36:30 +0800

    X.Helper新增Http类 准备使用HttpClient提供HTTP请求工具类

---
+++ Source/X.Helper/Http.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X.Helper
+{
+    public static class Http
+    {
+        internal static readonly HttpClient _HttpClient = new HttpClient();
+
+        
+    }
+}
