49e48710efa063d268014f7a03356a5c80382623
Systemx <607432@qq.com>
2025-05-26 22:00:03 +0800

    MultipartFormDataContent

--- Source/X.Helper.Http/Client_Content.cs
+++ Source/X.Helper.Http/Client_Content.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
 
 namespace X.Helper.Http
 {
@@ -17,52 +18,52 @@ namespace X.Helper.Http
         /// </summary>
         private void CreateContent()
         {
+            if (null == _ContentType)
+                _ContentType = new MediaTypeHeaderValue("text/html") { CharSet = _Encoding.WebName };
             var helper = new Helper.ContentHelper(_ContentType);
             //根据不同CONTENTTYPE类型处理对应CONTENT
             //未来考虑支持直接传入Stream对象
-            if (this._Files == null || this._Files.Count == 0)
+            if (this._Files == null || this._Files.Count == 0 || !_ContentType.MediaType.ToLower().Equals("multipart/form-data"))
             {
-                //未添加文件
-                switch(this._RequestContentType)
-                {
-                    case Enums.RequestContentType.STRING:
-                        this._ResponseMessage.Content = helper.GetStringContent(this._ContentString);
-                        break;
-                    case Enums.RequestContentType.DICTIONARY:
-                        this._ResponseMessage.Content = helper.GetStringContent(this._ContentDictionary);
-                        break;
-                    case Enums.RequestContentType.OBJECT:
-                        this._ResponseMessage.Content = helper.GetStringContent(this._ContentObject);
-                        break;
-                        default:
-                        //按STRING处理
-                        this._ResponseMessage.Content = helper.GetStringContent(this._ContentString);
-                        break;
-                }
-            }
-
-            //根据不同的CONTENTTYPE类型设置HEADERS的ContentType
-            //Multipart/Form-Data类型由MultipartFormDataContent设定？或者判断有文件要上传时手动设定
-            //考虑是否在上传文件时需要手动设置其它ContentType？
-            if (null == _ContentType)
-                _ContentType = new MediaTypeHeaderValue("text/html") { CharSet = _Encoding.WebName };
-            _RequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue(_ContentType.MediaType);
-            _RequestMessage.Content.Headers.ContentType.CharSet = _ContentType.CharSet;
-
-           
-           
-        }
-
-        private void CreateStringContent()
-        {
-            if (_RequestMessage.Content == null)
-                return;
-            if (_RequestMessage.Content.Headers.ContentType == null)
-            {
+                _RequestMessage.Content = GetStringContent();
+                //根据不同的CONTENTTYPE类型设置HEADERS的ContentType
                 _RequestMessage.Content.Headers.ContentType = new MediaTypeHeaderValue(_ContentType.MediaType);
                 _RequestMessage.Content.Headers.ContentType.CharSet = _ContentType.CharSet;
             }
-            _RequestMessage.Content = new StringContent("", _Encoding, _ContentType.MediaType);
+            else
+            {
+                //有文件或者Multipart/Form-Data类型
+
+
+                //Multipart/Form-Data类型由MultipartFormDataContent设定？或者判断有文件要上传时手动设定
+                //考虑是否在上传文件时需要手动设置其它ContentType？
+            }
+
+
+
+
+
+
         }
+
+        private HttpContent GetStringContent()
+        {
+            var helper = new Helper.ContentHelper(_ContentType);
+            switch (this._RequestContentType)
+            {
+                case Enums.RequestContentType.STRING:
+                    return helper.GetStringContent(this._ContentString);
+                case Enums.RequestContentType.DICTIONARY:
+                    return helper.GetStringContent(this._ContentDictionary);
+                    
+                case Enums.RequestContentType.OBJECT:
+                    return helper.GetStringContent(this._ContentObject);
+                    
+                default:
+                    //按STRING处理
+                    return helper.GetStringContent(this._ContentString);
+            }
+        }
+
     }
 }
